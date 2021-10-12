0df22532403b8fe0ac6f602ff1c5675cd88ae8d1
Lê Đức Phúc <58520153+PhucLD-Dev@users.noreply.github.com>
2021-10-12 16:58:02 +0700

    Update ApiManager.java

--- ASSIGNMENT/app/src/main/java/com/example/assignment/ApiManager.java
+++ ASSIGNMENT/app/src/main/java/com/example/assignment/ApiManager.java
@@ -7,9 +7,9 @@ import retrofit2.http.GET;
 
 public interface ApiManager {
     public static String BASE_URL = "http://dataservice.accuweather.com";
-    @GET("/forecasts/v1/hourly/12hour/353412?apikey=dql9qI0nWxJ4l6poDWErWfGJt1aazF0w&language=vi-vn&metric=true")
+    @GET("/forecasts/v1/hourly/12hour/353412?apikey=AHjIt54FCG9sQKKWQLlOSwg7SfgU8072&language=vi-vn&metric=true")
     Call<List<Wheather>> gethour();
 
-    @GET("/forecasts/v1/daily/5day/353412?apikey=dql9qI0nWxJ4l6poDWErWfGJt1aazF0w&language=vi-vn&metric=true")
+    @GET("/forecasts/v1/daily/5day/353412?apikey=AHjIt54FCG9sQKKWQLlOSwg7SfgU8072&language=vi-vn&metric=true")
     Call<List<Wheather>> getDay();
 }
